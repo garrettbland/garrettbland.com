36e52c642a08fe5b9cf1e98917328107ffc282c9
garrettbland <gmorganbland@gmail.com>
2021-06-27 22:14:22 -0500

    working on dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,9 @@ const colors = require('tailwindcss/colors')
 module.exports = {
     mode: 'jit',
     purge: ['./src/**/*.liquid'],
-    darkMode: false, // or 'media' or 'class'
+    darkMode: 'class',
     theme: {
         extend: {
-            rotate: {
-                25: '25deg',
-            },
             colors: {
                 ...colors,
             },
@@ -16,7 +13,49 @@ module.exports = {
                 sans: ['"Inter"'],
                 mono: ['"Fira Code"'],
             },
+            typography: (theme) => ({
+                dark: {
+                    css: {
+                        color: theme('colors.gray.300'),
+                        '[class~="lead"]': {
+                            color: theme('colors.gray.400'),
+                        },
+                        a: { color: theme('colors.gray.100') },
+                        strong: { color: theme('colors.gray.100') },
+                        'ul > li::before': {
+                            backgroundColor: theme('colors.gray.700'),
+                        },
+                        hr: { borderColor: theme('colors.gray.800') },
+                        blockquote: {
+                            color: theme('colors.gray.100'),
+                            borderLeftColor: theme('colors.gray.800'),
+                        },
+                        h1: { color: theme('colors.gray.100') },
+                        h2: { color: theme('colors.gray.100') },
+                        h3: { color: theme('colors.gray.100') },
+                        h4: { color: theme('colors.gray.100') },
+                        code: { color: theme('colors.gray.100') },
+                        'a code': { color: theme('colors.gray.100') },
+                        pre: {
+                            color: theme('colors.gray.200'),
+                            backgroundColor: theme('colors.gray.800'),
+                        },
+                        thead: {
+                            color: theme('colors.gray.100'),
+                            borderBottomColor:
+                                theme('colors.gray.700'),
+                        },
+                        'tbody tr': {
+                            borderBottomColor:
+                                theme('colors.gray.800'),
+                        },
+                    },
+                },
+            }),
         },
+    },
+    variants: {
+        typography: ['dark'],
     },
     plugins: [
         require('@tailwindcss/typography'),
