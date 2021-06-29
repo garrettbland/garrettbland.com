5538ff89c0ea16cc419fe6a5bcddcceda6fec220
garrettbland <gmorganbland@gmail.com>
2021-06-29 00:05:11 -0500

    working on notion

--- src/_data/posts.js
+++ src/_data/posts.js
@@ -1,12 +1,51 @@
+require('dotenv').config()
 const chalk = require('chalk')
 const { Client } = require('@notionhq/client')
+
+// Initialize notion client
+const notion = new Client({
+    auth: process.env.NOTION_API_KEY,
+})
+const databaseId = process.env.NOTION_DATABASE_ID
 
 module.exports = async function () {
     console.log(chalk.green('🚚 Getting posts from Notion...'))
 
-    return [
-        {
-            title: 'Title of post',
+    /**
+     * Fetch list of posts from notion database and
+     * where status is published & category is post
+     */
+    const response = await notion.databases.query({
+        database_id: databaseId,
+        filter: {
+            and: [
+                {
+                    property: 'Status',
+                    select: {
+                        equals: 'Published',
+                    },
+                },
+                {
+                    property: 'Category',
+                    select: {
+                        equals: 'Post',
+                    },
+                },
+            ],
         },
-    ]
+        sorts: [
+            {
+                property: 'Published',
+                direction: 'ascending',
+            },
+        ],
+    })
+
+    console.log(
+        chalk.green(
+            `📝 Retrieved ${response.results.length} posts...`
+        )
+    )
+
+    return response.results
 }
